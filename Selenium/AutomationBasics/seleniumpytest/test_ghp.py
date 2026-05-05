0070778e54e20fb2120af86afd2e250daf50a3be
Sh-singh3009 <shauryansh3009@gmail.com>
2026-05-05 18:17:47 +0530

    Selenium Pytest Basics Commit

---
+++ Selenium/AutomationBasics/seleniumpytest/test_ghp.py
@@ -0,0 +1,40 @@
+# import time
+
+import pytest
+from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.wait import WebDriverWait
+# from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+import pytest_check as check
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.get('https://www.google.com')
+    yield driver
+    driver.quit()
+
+def test_ghpload(driver):
+    pagetitle = driver.title
+    assert pagetitle == 'Google', 'Google Home Page Not Loaded'
+
+def test_imagespageload(driver):
+    driver.find_element(By.LINK_TEXT, "Images").click()
+    pagetitle = driver.title
+    assert pagetitle == 'Google Images', 'Images Page Not Loaded'
+
+def test_businesslink(driver):
+    driver.find_element(By.LINK_TEXT, 'Business').click()
+    wait = WebDriverWait(driver,5)
+    wait.until(EC.title_contains('Business'))
+    # assert 'Business' in driver.title, 'Business Page Not Loaded - Title Check'
+    # assert 'business' in driver.current_url, 'Business Page Not Loaded - URL Check'
+    check.is_in('Business', driver.title,  'Business Page Not Loaded - Title Check')
+    check.is_in('business', driver.current_url, 'Business Page Not Loaded - URL Check')
+
